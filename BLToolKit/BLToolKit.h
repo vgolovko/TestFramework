53d40a3bbe3cd14a7db82a69733d1d6834320c00
Vitaliy Golovko <vitaliy.golovko@windmill.com>
2020-07-30 16:13:15 +0300

    Initial Commit

---
+++ BLToolKit/BLToolKit.h
@@ -0,0 +1,19 @@
+//
+//  BLToolKit.h
+//  BLToolKit
+//
+//  Created by Vitaliy Golovko on 30.07.2020.
+//  Copyright © 2020 windmill. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BLToolKit.
+FOUNDATION_EXPORT double BLToolKitVersionNumber;
+
+//! Project version string for BLToolKit.
+FOUNDATION_EXPORT const unsigned char BLToolKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BLToolKit/PublicHeader.h>
+
+
